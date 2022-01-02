3a8a98fb7e460e9240124c54845eb4b30ec54650
MrOMadness <nicholas.stanciosaka@gmail.com>
2022-01-03 01:41:22 +0700

    Update Design

--- src/scripts/templates/modal.js
+++ src/scripts/templates/modal.js
@@ -8,19 +8,20 @@ const fetchApiModal = () => `
                 </div>
                 <div class="modal-body">
                     <div class="form-group row">
-                        <label for="fetchApiModalEndpoint" class="col-sm-2 col-form-label">Endpoint</label>
+                        <label for="fetchApiModalEndpoint" class="col-sm-2 col-form-label"><b>Endpoint</b></label>
                         <div class="col-sm-10">
                             <input type="text" class="form-control" id="fetchApiModalEndpoint" placeholder="/api/weather">
                         </div>
                     </div>
-                    <div class="form-group row mt-3">
-                        <label for="fetchApiModalMethod" class="col-sm-2 col-form-label">Method</label>
+                    <div class="form-group row mt-3 pb-3">
+                        <label for="fetchApiModalMethod" class="col-sm-2 col-form-label"><b>Method</b></label>
                         <div class="col-sm-10">
                             <input type="text" class="form-control" id="fetchApiModalMethod" placeholder="POST">
                         </div>
                     </div>
-                    <div class="form-group mt-3">
-                        <label for="fetchApiModalBody">Body</label>
+                    <hr class="mb-3">
+                    <div class="form-group">
+                        <label for="fetchApiModalBody"><b>Body</b></label>
                         <textarea class="form-control mt-2" id="fetchApiModalBody" rows="3" placeholder='{"city": "palembang"}'></textarea>
                     </div>
                 </div>
